3a7420d5b158acdea84dd68efff45bbe80860704
quentintyr <quentin.tyr@gmail.com>
2024-07-02 16:41:51 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron/main')
+const path = require('node:path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), 
+      nodeIntegration: true,
+    }
+  })
+
+  win.loadFile('html/index.html')
+  // win.setMenu(null)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'win32') {
+    app.quit()
+  }
+})
